d2ab84049c513d91b4c0348f782e2c772e2b134b
Alicja Popielarska <popielarska.alicja@gmail.com>
2023-03-03 20:47:44 +0100

    semi-finished code

--- 5_3.py
+++ 5_3.py
@@ -1,21 +1,39 @@
-class AddressBook:
-    def __init__(self, first_name, last_name, company, position, email_address):
+
+class BaseContact:
+    def __init__(self, first_name, last_name, phone_private, email_address):
         self.first_name = first_name
         self.last_name = last_name
-        self.company = company
-        self.position = position
+        self.phone_private = phone_private
         self.email_address = email_address
     def __str__(self):
-        return f"{self.first_name}, {self.last_name}, {self.company}, {self.email_address}"
+        return f"{self.first_name}, {self.last_name}, {self.phone_private}, {self.email_address}"
     def contact(self):
-        return f"Kontaktuję się z {self.first_name} {self.last_name}, {self.position}, {self.email_address}"
-    
+        return f"Wybieram numer {self.phone_private} i dzwonię do {self.first_name} {self.last_name}"
+    @property
+    def label_length(self):
+        return len(self.first_name) + len(self.last_name)
+
+class BusinessContact(BaseContact):
+    def __init__(self, company, position, phone_company, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.company = company
+        self.position = position
+        self.phone_company = phone_company
+    def __str__(self):
+        return f"{self.first_name}, {self.last_name}, {self.position}, {self.company}, {self.phone_company}, {self.email_address}"
+    def contact(self):
+        return f"Wybieram numer {self.phone_company} i dzwonię do {self.first_name} {self.last_name}"
+    @property
+    def label_length(self):
+        return len(self.first_name) + len(self.last_name)
 
 
-BC1=AddressBook(first_name="Dean", last_name="Gonzalez", company="Martin-Martin", position="Engineer", email_address="Dean.Gonzalez@M-M.com")
-BC2=AddressBook(first_name="James", last_name="Smith", company="Smith Inc.", position="Regulatory Affairs Officer", email_address="J.Smith@smith.com")
-BC3=AddressBook(first_name="Zachary", last_name="Salinas", company="Cook, Bauer and Strickland", position="Scientist, research (medical)", email_address="Zachary.Salinas@CBS.com")
-BC4=AddressBook(first_name="Andrew", last_name="Nguyen", company="Dorsey, Nunez and Alvarez", position="Make", email_address="A.Nguyen@DN&A.com")
-BC5=AddressBook(first_name="Elizabeth", last_name="Jones", company="Campbell, Holland and Smith", position="Designer, multimedia", email_address="E.Jones@C-H-S.com")
+BC1=BusinessContact(first_name="Dean", last_name="Gonzalez", phone_private="+48123123123",email_address="Dean.Gonzalez@M-M.com", company="Martin-Martin", position="Engineer", phone_company="+48987987987")
+BC2=BusinessContact(first_name="James", last_name="Smith", phone_private="+48234234234", email_address="J.Smith@smith.com", company="Smith Inc.", position="Regulatory Affairs Officer", phone_company="+48876876876")
+BC3=BusinessContact(first_name="Zachary", last_name="Salinas", phone_private="+48345345345", email_address="Zachary.Salinas@CBS.com", company="Cook, Bauer and Strickland", position="Scientist, research (medical)", phone_company="+48765765765")
+BC4=BusinessContact(first_name="Andrew", last_name="Nguyen", phone_private="+48456456456", email_address="A.Nguyen@DN&A.com", company="Dorsey, Nunez and Alvarez", position="Make", phone_company="+48654654654")
+BC5=BusinessContact(first_name="Elizabeth", last_name="Jones", phone_private="+48567567567", email_address="E.Jones@C-H-S.com", company="Campbell, Holland and Smith", position="Designer, multimedia", phone_company="+48543543543")
 
 BCs=[BC1, BC2, BC3, BC4, BC5]
+print(BC1.label_length())
+
